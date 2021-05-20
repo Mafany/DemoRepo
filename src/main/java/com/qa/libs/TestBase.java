0f590b93be7db3719a6102263de1b22f8c4a30a9
Mafany <molamaf@gmail.com>
2021-05-20 10:25:20 +0100

    This is  first  git commit for my selenium training

---
+++ src/main/java/com/qa/libs/TestBase.java
@@ -0,0 +1,11 @@
+package com.qa.libs;
+
+public class TestBase {
+	
+	public void  test() 
+	{
+		 System.out.print(" print  something on the console");
+		
+		
+	}
+}
---
+++ src/main/java/com/qa/utils/TestUtil.java
@@ -0,0 +1,12 @@
+package com.qa.utils;
+
+public class TestUtil {
+	
+	public void testUtil()
+	{
+		
+		System.out.println("This  is the  testUtil method");
+		
+	}
+
+}
